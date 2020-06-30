7c8c7605646537a9e73efb5f3b302894348714cb
Azza  BenSliman <abensliman@mail.arsd.dc>
2020-06-30 15:59:57 +0100

    100DaysOfCode: Vanilla JS API Validation

--- controllers/user.js
+++ controllers/user.js
@@ -9,15 +9,71 @@ const userController = {
       .json({ success: true, message: `Job well done ${id}` });
   },
   createUser: (req, res, next) => {
-    const { id, firstName, lastName } = req.body;
+    const { firstName, lastName, email, userType } = req.body;
+
+    // Validate with Vanilla JavaScript functions !!
+    let errors = {};
+    let errorMessage = "";
+    let isBodyValid = true;
+
+    if (
+      !firstName ||
+      typeof firstName !== "string" ||
+      firstName.trim().length === 0
+    ) {
+      isBodyValid = false;
+      errors["firstName"] = "This should be a string & not empty";
+    }
+
+    if (
+      !lastName ||
+      typeof lastName !== "string" ||
+      lastName.trim().length === 0
+    ) {
+      isBodyValid = false;
+      errors["lastName"] = "This should be a string & not empty";
+    }
+
+    if (!email || typeof email !== "string" || email.trim().length === 0) {
+      isBodyValid = false;
+      errors["email"] = "This should be a string & not empty";
+    }
+
+    if (
+      !userType ||
+      typeof userType !== "string" ||
+      userType.trim().length === 0
+    ) {
+      isBodyValid = false;
+      errors["userType"] = "This should be a string & not empty";
+    } else {
+      const checks = ["user", "admin", "support"];
+      const found = checks.some((check) => check === userType);
+      if (!found) {
+        isBodyValid = false;
+        errors["userType"] =
+          "This should be a string & can have these values " + checks.join(".");
+      }
+    }
+
+    if (!isBodyValid) {
+      return res.status(400).json({
+        success: isBodyValid,
+        errorMessage: "Kindly correct the error(s)",
+        errors,
+      });
+    }
+
+    // only execute bellow after validating data
     const userPayload = {
-      id,
       firstName,
       lastName,
+      email,
+      userType,
     };
     return res.status(200).json({
       success: true,
-      message: `Job well done ${id}`,
+      message: `Job well done ${firstName}`,
       //Data is equal to userPayload
       data: userPayload,
     });
