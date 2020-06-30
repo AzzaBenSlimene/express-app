606852df833b5c364b54362d963ea78aa6184fe2
Azza  BenSliman <abensliman@mail.arsd.dc>
2020-06-30 16:01:23 +0100

    100DaysOfCode: Vanilla JS API Validation

--- controllers/user.js
+++ controllers/user.js
@@ -11,58 +11,58 @@ const userController = {
   createUser: (req, res, next) => {
     const { firstName, lastName, email, userType } = req.body;
 
-    // Validate with Vanilla JavaScript functions !!
-    let errors = {};
-    let errorMessage = "";
-    let isBodyValid = true;
+    // // Validate with Vanilla JavaScript functions !!
+    // let errors = {};
+    // let errorMessage = "";
+    // let isBodyValid = true;
 
-    if (
-      !firstName ||
-      typeof firstName !== "string" ||
-      firstName.trim().length === 0
-    ) {
-      isBodyValid = false;
-      errors["firstName"] = "This should be a string & not empty";
-    }
+    // if (
+    //   !firstName ||
+    //   typeof firstName !== "string" ||
+    //   firstName.trim().length === 0
+    // ) {
+    //   isBodyValid = false;
+    //   errors["firstName"] = "This should be a string & not empty";
+    // }
 
-    if (
-      !lastName ||
-      typeof lastName !== "string" ||
-      lastName.trim().length === 0
-    ) {
-      isBodyValid = false;
-      errors["lastName"] = "This should be a string & not empty";
-    }
+    // if (
+    //   !lastName ||
+    //   typeof lastName !== "string" ||
+    //   lastName.trim().length === 0
+    // ) {
+    //   isBodyValid = false;
+    //   errors["lastName"] = "This should be a string & not empty";
+    // }
 
-    if (!email || typeof email !== "string" || email.trim().length === 0) {
-      isBodyValid = false;
-      errors["email"] = "This should be a string & not empty";
-    }
+    // if (!email || typeof email !== "string" || email.trim().length === 0) {
+    //   isBodyValid = false;
+    //   errors["email"] = "This should be a string & not empty";
+    // }
 
-    if (
-      !userType ||
-      typeof userType !== "string" ||
-      userType.trim().length === 0
-    ) {
-      isBodyValid = false;
-      errors["userType"] = "This should be a string & not empty";
-    } else {
-      const checks = ["user", "admin", "support"];
-      const found = checks.some((check) => check === userType);
-      if (!found) {
-        isBodyValid = false;
-        errors["userType"] =
-          "This should be a string & can have these values " + checks.join(".");
-      }
-    }
+    // if (
+    //   !userType ||
+    //   typeof userType !== "string" ||
+    //   userType.trim().length === 0
+    // ) {
+    //   isBodyValid = false;
+    //   errors["userType"] = "This should be a string & not empty";
+    // } else {
+    //   const checks = ["user", "admin", "support"];
+    //   const found = checks.some((check) => check === userType);
+    //   if (!found) {
+    //     isBodyValid = false;
+    //     errors["userType"] =
+    //       "This should be a string & can have these values " + checks.join(".");
+    //   }
+    // }
 
-    if (!isBodyValid) {
-      return res.status(400).json({
-        success: isBodyValid,
-        errorMessage: "Kindly correct the error(s)",
-        errors,
-      });
-    }
+    // if (!isBodyValid) {
+    //   return res.status(400).json({
+    //     success: isBodyValid,
+    //     errorMessage: "Kindly correct the error(s)",
+    //     errors,
+    //   });
+    // }
 
     // only execute bellow after validating data
     const userPayload = {
